425657efe0641d342d1cfc77c4f04c05b60a4f9c
Owen Shepherd <owen@owen.cafe>
2020-10-02 16:12:21 +0100

    removed local-shellrc

--- zsh.nix
+++ zsh.nix
@@ -16,7 +16,6 @@ in
     initExtra = ''
       eval "$(${pkgs.fasd}/bin/fasd --init auto)"
       . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-      . ${"${(builtins.fetchGit { url = "https://github.com/414owen/local-shellrc"; }).outPath}/source-local-shellrc.zsh"}
       ${builtins.readFile (./shell.zsh)}
       ${builtins.readFile (./fns.sh)}
       ${import ./fns.nix { pkgs = pkgs; }}
