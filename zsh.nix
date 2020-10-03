96a8f299e273984ee068a5540dedb12e0d6ccdc0
Owen Shepherd <owen@owen.cafe>
2020-10-03 22:54:32 +0100

    start tmux on zsh init

--- zsh.nix
+++ zsh.nix
@@ -14,11 +14,15 @@ in
       share = true;
     };
     initExtra = ''
-      eval "$(${pkgs.fasd}/bin/fasd --init auto)"
-      . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-      ${builtins.readFile (./shell.zsh)}
-      ${builtins.readFile (./fns.sh)}
-      ${import ./fns.nix { pkgs = pkgs; }}
+      if [ -z "$TMUX" ]; then
+        tmux
+      else
+        eval "$(${pkgs.fasd}/bin/fasd --init auto)"
+        . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        ${builtins.readFile (./shell.zsh)}
+        ${builtins.readFile (./fns.sh)}
+        ${import ./fns.nix { pkgs = pkgs; }}
+      fi
     '';
     sessionVariables = import ./env.nix { pkgs = pkgs; };
     shellAliases = aliases;
