ce246c299f6288a087acc058777e6d9dddc5acd0
Owen Shepherd <owen@owen.cafe>
2021-03-16 11:10:07 +0000

    kakoune stuff

--- overlays/kakounePlugins.nix
+++ overlays/kakounePlugins.nix
@@ -1,5 +1,6 @@
 self: super: {
   kakounePlugins = super.kakounePlugins // {
     kakoune-easymotion = super.callPackage ../derivations/kakoune-easymotion.nix {};
+    kakboard = super.callPackage ../derivations/kakboard.nix {};
   };
 }
