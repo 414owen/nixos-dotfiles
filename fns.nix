47585aae4322a23d112902dcb7ce259f8d7630d0
Owen Shepherd <owen@owen.cafe>
2021-04-22 11:25:15 +0100

    cd -> pushd

--- fns.nix
+++ fns.nix
@@ -111,4 +111,28 @@ try() {
   fi
   nix-shell -p "$1" --run "$2"
 }
+
+function cd() {
+  if [ "$#" = "0" ]
+  then
+  pushd "$HOME" > /dev/null
+  elif [ -f "$1" ]
+  then
+    "$EDITOR" "$1"
+  else
+  pushd "$1" > /dev/null
+  fi
+}
+
+function bd(){
+  if [ "$#" = "0" ]
+  then
+    popd > /dev/null
+  else
+    for i in $(seq "$1")
+    do
+      popd > /dev/null
+    done
+  fi
+}
 ''
