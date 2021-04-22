21ed090ee19cdd5c30af118981ceb6bb260433a1
Owen Shepherd <owen@owen.cafe>
2021-04-22 11:22:16 +0100

    add cdpath

--- home/env.nix
+++ home/env.nix
@@ -7,14 +7,15 @@ let
 in
 
 {
-  LS_COLORS = "di=33:ow=33:ln=target:ex=35:fi=34";
-  EDITOR = defaults.editor;
   AWS_OKTA_BACKEND = "secret-service";
+  CDPATH = ".:~/src";
   DOTFILE = "$0";
-  FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview '${pkgs.bat}/bin/bat --color=always --style=header,grid --line-range :300 {}'";
-  FZF_DEFAULT_COMMAND = "${fzf-command}";
-  FZF_CTRL_T_COMMAND = "${fzf-command}";
+  EDITOR = defaults.editor;
   FZF_CTRL_R_OPTS = "--preview='' --preview-window 'right:0%'";
+  FZF_CTRL_T_COMMAND = "${fzf-command}";
+  FZF_DEFAULT_COMMAND = "${fzf-command}";
+  FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview '${pkgs.bat}/bin/bat --color=always --style=header,grid --line-range :300 {}'";
+  LS_COLORS = "di=33:ow=33:ln=target:ex=35:fi=34";
   MAKEFLAGS = "-j$(nproc)";
   TERMINAL = "${pkgs.st}/bin/st";
 }
