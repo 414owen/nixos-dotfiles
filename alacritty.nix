6ce29acebb19a81af72ab2ca905ad6964b0696d6
Owen Shepherd <oshepherd@symmetryinvestments.com>
2021-06-12 18:20:27 +0100

    update

--- alacritty.nix
+++ alacritty.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+let
+  famObj = { family = "Fira Mono"; };
+in
+
 {
   programs.alacritty = {
     settings = {
@@ -9,6 +13,13 @@
       };
       background_opacity = 0.9;
       live_config_reload = false;
+      font = {
+        normal = famObj;
+        bold = famObj;
+        italic = famObj;
+        bold_italic = famObj;
+        size = 14;
+      };
     };
   };
 }
