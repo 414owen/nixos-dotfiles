2f162cdd86f1234d994e944bf77a7f3a3dfa97f4
Owen Shepherd <oshepherd@symmetryinvestments.com>
2021-11-05 14:27:09 +0000

    working emacs

--- env.nix
+++ env.nix
@@ -18,11 +18,11 @@ in
   MAKEFLAGS = "-j$(nproc)";
   TERMINAL = "${pkgs.st}/bin/st";
   # force all generic backend to use wayland backend
-  GDK_BACKEND = "wayland";
-  QT_QPA_PLATFORM = "wayland-egl";
-  CLUTTER_BACKEND = "wayland";
-  SDL_VIDEODRIVER = "wayland";
-  EWOL_BACKEND = "wayland";
+  # GDK_BACKEND = "wayland";
+  # QT_QPA_PLATFORM = "wayland-egl";
+  # CLUTTER_BACKEND = "wayland";
+  # SDL_VIDEODRIVER = "wayland";
+  # EWOL_BACKEND = "wayland";
   #
   # XCURSOR_THEME = "Quogir-dark";
 }
