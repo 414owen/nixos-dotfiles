bfee198588eac9336c823c6f5dcf1984c5f48ac0
Owen Shepherd <oshepherd@symmetryinvestments.com>
2021-11-24 13:56:28 +0000

    copy script

--- alacritty.nix
+++ alacritty.nix
@@ -12,7 +12,7 @@ in
         decorations = "none";
         dynamic_title = true;
       };
-      background_opacity = 0.9;
+      background_opacity = 1;
       live_config_reload = false;
     } // theme;
   };
