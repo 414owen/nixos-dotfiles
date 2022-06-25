371988f0271869b09275a3b6d6453e9dced03b5f
Owen Shepherd <owen.shepherd@tiko.energy>
2022-06-25 11:59:47 +0100

    Modify nixos config for new work laptop

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,24 +8,19 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c1a5d6fe-043b-438d-ac70-fc906e1ae7a0";
+    { device = "/dev/disk/by-uuid/b4443c16-86c1-42af-bc2c-349bf3165163";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/1c3e9f47-41dd-4ef5-a8e3-486f63660090";
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/d0a0c5ed-40d6-4dd0-a014-b2b89ce3be60";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0803-2FD1";
-      fsType = "vfat";
-    };
-
-  swapDevices = [ { device = "/swapfile"; size = 16384; } ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -34,6 +29,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
