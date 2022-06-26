ebab30af8ee87b437e2a424c6232126c0c540796
Owen Shepherd <owen.shepherd@tiko.energy>
2022-06-26 10:29:03 +0100

    Use home dotfiles

--- overlays/kakounePlugins.nix
+++
@@ -1,6 +0,0 @@
-self: super: {
-  kakounePlugins = super.kakounePlugins // {
-    kakoune-easymotion = super.callPackage ../derivations/kakoune-easymotion.nix {};
-    kakboard = super.callPackage ../derivations/kakboard.nix {};
-  };
-}
