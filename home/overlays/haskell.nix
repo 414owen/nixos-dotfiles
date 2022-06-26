8e65945b739fe7b774a06787ff71b3da446b77d2
Owen Shepherd <owen.shepherd@tiko.energy>
2022-06-26 15:01:00 +0100

    Remove unused stuff

--- home/overlays/haskell.nix
+++
@@ -1,3 +0,0 @@
-self: super: {
-  # pp-ghci = with super.haskell.lib; doJailbreak (dontHaddock (super.haskellPackages.callCabal2nix "ghci-pretty" (import ../nix/sources.nix).pretty-ghci {}));
-}
--- home/overlays/zoom.nix
+++
@@ -1,7 +0,0 @@
-self: super: {
-  zoom = super.zoom.overrideAttrs (old: {
-    postFixup = old.postFixup + ''
-      wrapProgram $out/bin/zoom-us --unset XDG_SESSION_TYPE
-    '';
-  });
-}
