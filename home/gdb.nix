60f83f2f3130ebbc559d7717fa411877e36bbda3
Owen Shepherd <owen.shepherd@tiko.energy>
2023-10-09 11:41:51 +0200

    feat: gdb history

--- home/gdb.nix
+++ home/gdb.nix
@@ -1,8 +1,12 @@
 { home, ... }:
 
 {
-    home.file.".gdbinit".text = ''
-       set print demangle
-       focus next
-    '';
+  home.file.".gdbinit".text = ''
+     set print demangle
+     focus next
+     set history filename /home/owen/.gdb_history
+     set history save on
+     set history size 1024
+     set history remove-duplicates unlimited
+  '';
 }
